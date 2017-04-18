4caf33aad54c0ffec4a83ea3248fd1814edd0ef8
Fredrick-Tam <tamf295@yahoo.com>
2017-04-18 02:52:03 -0400

    home screen initial UI changes

--- dine@CU/app/src/main/java/com/example/fredrick_tam/dinecu/Dining_Halls.java
+++ dine@CU/app/src/main/java/com/example/fredrick_tam/dinecu/Dining_Halls.java
@@ -1,18 +1,19 @@
 package com.example.fredrick_tam.dinecu;
 
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.roughike.bottombar.BottomBar;
+
+@SuppressWarnings("ConstantConditions")
 public class Dining_Halls extends AppCompatActivity {
+    private BottomBar mBottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dining__halls);
         //noinspection deprecation
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.dining)));
-
         // making bottom bar
 
 
