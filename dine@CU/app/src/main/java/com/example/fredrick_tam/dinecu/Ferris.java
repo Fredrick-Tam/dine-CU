a660a53fa3ae23846d78df999a04bbe66c8b0e00
Brian Tsau <bt2420@columbia.edu>
2017-04-30 11:28:15 -0400

    Revert "added extra files for web scraping"
    This reverts commit 8bcca4c6bf625c7b4431a8ab55fe58fb08606d8a.

--- dine@CU/app/src/main/java/com/example/fredrick_tam/dinecu/Ferris.java
+++ dine@CU/app/src/main/java/com/example/fredrick_tam/dinecu/Ferris.java
@@ -1,142 +1,14 @@
 package com.example.fredrick_tam.dinecu;
 
-import java.util.*;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.os.AsyncTask;
-import android.util.Log;
-import android.widget.ListView;
-
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-
-
-
 public class Ferris extends AppCompatActivity {
-
-    public Document htmlDocument;
-    public String htmlPageUrl = "http://dining.columbia.edu/?quicktabs_homepage_menus_quicktabs=0#quicktabs-homepage_menus_quicktabs";
-    public ArrayList<String> menuItemNames = new ArrayList<String>();
-    public ArrayList<String> menuItemImage = new ArrayList<String>();
-    public String URL;
-    public ListView list;
-    public String[] nameList;
-    public Bitmap[] imageList;
-    public ArrayList<Bitmap> mapArray = new ArrayList<Bitmap>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setTitle(R.string.Ferris);
         setContentView(R.layout.activity_ferris);
-
-        GetXMLTask ferris_XML = new GetXMLTask();
-        ferris_XML.execute(new String[] { URL });
-
-    }
-
-    public class GetXMLTask extends AsyncTask<String, Void, Bitmap> {
-        @Override
-        protected Bitmap doInBackground(String... urls) {
-            try {
-                htmlDocument = Jsoup.connect(htmlPageUrl).get();
-                if (htmlDocument.hasClass("views-field-field-meal-images-fid")){
-                    Elements menu_items = htmlDocument.getElementsByClass("views-field-field-meal-images-fid");
-
-                    for (Element menu_item : menu_items) {
-                        Element link = menu_item.select("a").first();
-                        if (link != null){
-                            menuItemNames.add(link.attr("title"));
-                            menuItemImage.add(link.attr("href"));
-                        }
-                    }
-                } else{
-                    menuItemNames.add("This dining hall is currently closed.");
-                    menuItemImage.add("https://www.jewishchattanooga.com/wp-content/uploads/Sorry-were-closed-sign.jpg");
-                }
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            Bitmap map = null;
-            for (String url : menuItemImage) {
-                map = downloadImage(url);
-                mapArray.add(map);
-            }
-            return map;
-
-        }
-
-        // Sets the Bitmap returned by doInBackground
-        @Override
-        protected void onPostExecute(Bitmap result) {
-
-            imageList = mapArray.toArray(new Bitmap[mapArray.size()]);
-            nameList = menuItemNames.toArray(new String[menuItemNames.size()]);
-            for (Bitmap img : imageList){
-                Log.d("bitmap", String.valueOf(img));
-            }
-
-            CustomListAdapter adapter=new CustomListAdapter(Ferris.this, nameList, imageList);
-            list=(ListView)findViewById(R.id.list);
-            list.setAdapter(adapter);
-
-        }
-
-        // Creates Bitmap from InputStream and returns it
-        public Bitmap downloadImage(String url) {
-            Bitmap bitmap = null;
-            InputStream stream = null;
-            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-            bmOptions.inSampleSize = 1;
-
-            try {
-                stream = getHttpConnection(url);
-                bitmap = BitmapFactory.
-                        decodeStream(stream, null, bmOptions);
-                stream.close();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-            return bitmap;
-        }
-
-        // Makes HttpURLConnection and returns InputStream
-        public InputStream getHttpConnection(String urlString)
-                throws IOException {
-            InputStream stream = null;
-            URL url = new URL(urlString);
-            URLConnection connection = url.openConnection();
-
-            try {
-                HttpURLConnection httpConnection = (HttpURLConnection) connection;
-                httpConnection.setRequestMethod("GET");
-                httpConnection.connect();
-
-                if (httpConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                    stream = httpConnection.getInputStream();
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            return stream;
-        }
-
     }
 }
